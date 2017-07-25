0c4654c4fbca895eccd2cc103e854a465133fdf0
Shahzaib Ather <shahzaibather7@github.ca>
2017-07-25 10:36:23 -0700

    Initial Commit

---
+++ Assignment 1 -Boxes (*P)/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment 1 -Boxes (*P)
+//
+//  Created by Mohammad Shahzaib Ather on 2017-07-25.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
